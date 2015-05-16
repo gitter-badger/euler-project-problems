9c89a7f8133dd45dcb27bdf56bd3dcd8c4da36f7
Frozen Mage <ironmusician@gmail.com>
2015-05-17 01:30:51 +0400

    Update problem2

--- Problem-2/problem2.cpp
+++ Problem-2/problem2.cpp
@@ -1,8 +1,23 @@
+/*
+*	Please try to do it yourself until deciding to look at this code.
+*	I am very grateful of Project Euler members who drive all of this.
+*	This is just my repo of those problems that I chose to solve.
+*	Please don't just copy. Because that won't give you anything.
+*	Try and do it yourself and find the joy behind.
+*	I hope you understand everything in this file and in my code.
+*	I will decide what should be commented and needn't.
+*	
+*	Thanks!
+*	
+*	Tweet:	@ironmusician
+*	E-mail: ironmusician@gmail.com
+*/
 #include <iostream>
 
-using namespace std;
-
 int main(void){
+	
+	using std::cout;
+	using std::endl;
 
 	long sumOfEvens = 0;
 	long localSum = 0;
